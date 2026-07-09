a5045309c50a58abd793cd5ca564a77fa8491491
pavanchaudhar <pavanchaudhari035@gmail.com>
2026-07-09 09:40:42 +0530

    update the home not found problem

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
 
-    path('home', views.home, name='home'),
+    path('', views.home, name='home'),
 
     path('students', views.student_list, name='student_list'),
 
