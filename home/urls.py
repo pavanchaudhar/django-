2c2372bf4abad655fffde423834a107a55a0198a
pavanchaudhar <pavanchaudhari035@gmail.com>
2026-07-09 15:44:13 +0530

    test for git commit

--- home/urls.py
+++ home/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('students', views.student_list, name='student_list'),
 
     path('add-student', views.add_student, name='add_student'),
-
+# test comments for git push
     
 
 ]
